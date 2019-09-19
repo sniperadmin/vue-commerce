8d45f1bbec10f4855a3c146e06e2f8de4b8616df
Nasr Galal <nasrbue@gmail.com>
2019-09-19 09:31:01 +0200

    fixed admin login page and redirection to portal

--- src/components/admin/AdminLogin.vue
+++ src/components/admin/AdminLogin.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="adminlogin mt-5 pt-5 h-100 container">
-
     <section class="form-gradient">
       <mdb-row class="justify-content-md-center">
         <mdb-col md="5">
-          <mdb-card>
+          <!-- login for admins -->
+          <mdb-card v-if="!isAdmin">
             <div class="header pt-3 peach-gradient">
               <mdb-row class="d-flex justify-content-center">
                 <h3 class="white-text mb-3 pt-3 font-weight-bold"> {{ $t('adminlogin.title') }}</h3>
               </mdb-row>
             </div>
+            <!-- card-body -->
             <mdb-card-body class="mx-4 mt-4">
+              <!-- form starts here -->
               <form method="POST" @submit.prevent="login">
-
               <mdb-input v-model="email" :placeholder="$t('adminlogin.email')" type="email" outline />
               <mdb-input v-model="password" :placeholder="$t('adminlogin.password')" type="password" outline containerClass="mb-0"/>
               <mdb-row class="d-flex align-items-center mb-4 mt-5">
@@ -26,12 +27,24 @@
               </mdb-row>
                 <p class="red-text">{{ boo }}</p>
               </form>
+              <!-- form ends here -->
+            </mdb-card-body><!-- ./card-body -->
+          </mdb-card>
+
+          <!-- this is just to see if admin could not be redirected for some reason -->
+          <mdb-card v-if="isAdmin">
+            <mdb-row>
+              <h2 class="h2-responsive">Welcome Admin</h2>
+            </mdb-row>
+            <mdb-card-body>
+              if you see this page, then contact us as soon as possible to fix it
             </mdb-card-body>
           </mdb-card>
+          <!-- this is just to see if admin could not be redirected for some reason -->
+          
         </mdb-col>
       </mdb-row>
     </section>
-
   </div>
 </template>
 
@@ -69,6 +82,7 @@
         password: null,
         isAdmin: false,
         boo: '',
+        isAdmin: false,
       }
     },
     methods: {
@@ -79,17 +93,16 @@
         console.log(email)
 
         fbAuth.auth().signInWithEmailAndPassword(email, password)
-          .then((user) => {
-            console.log(user)
+          .then(user => {
             this.$router.push(`/${i18n.locale}/admin`)
             this.$Progress.finish();
-            //   Swal.fire({
-            //   position: 'top-end',
-            //   type: 'success',
-            //   title: 'Logged in successfully',
-            //   showConfirmButton: false,
-            //   timer: 1500
-            // });
+              Swal.fire({
+              position: 'top-end',
+              type: 'success',
+              title: 'Logged in successfully',
+              showConfirmButton: false,
+              timer: 1500
+            });
 
           })
           .catch(error => {
@@ -110,8 +123,32 @@
             // [END_EXCLUDE]
           });
       },
+    },
+    created() {
+      fbAuth.auth().onAuthStateChanged(user => {
+        user.getIdTokenResult().then(idTokenResult => {
+          this.$Progress.start();
+          user.admin = idTokenResult.claims.admin;
+          this.isAdmin = user.admin
+              if (user.admin != true) {
+                this.$Progress.fail();
+                Swal.fire({
+                position: 'top-end',
+                type: 'warning',
+                title: 'restricted access',
+                showConfirmButton: false,
+                timer: 1500
+              });
+                this.$router.push(`/`);
+              } else {
+                  this.$Progress.start();
+                  this.$router.push(`/${i18n.locale}/admin`);
+                  this.$Progress.finish();
+              }
+        });
+      });
     }
-  };
+  }
 </script>
 
 <style scoped>
